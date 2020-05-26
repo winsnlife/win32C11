ed4bffdd87b42d6603b046d4c23f49856350cca0
qqa <542461965@qq.com>
2020-05-26 11:46:21 +0800

    '注释'

---
+++ Dll1C11/pch.cpp
@@ -0,0 +1,15 @@
+﻿// pch.cpp: 与预编译标头对应的源文件
+
+#include "pch.h"
+
+// 当使用预编译的头时，需要使用此源文件，编译才能成功。
+
+
+void testAuto() {
+
+	auto a = 5;
+	cout << "short  a :" << a << endl;
+	auto pi = new auto(2);
+
+	cout << "short  pi :" << pi << endl;
+}
--- win32C11/win32C11.cpp
+++ win32C11/win32C11.cpp
@@ -2,10 +2,12 @@
 //
 
 #include <iostream>
+#include "pch.h"
 
 int main()
 {
     std::cout << "Hello World!\n";
+	testAuto();
 }
 
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
